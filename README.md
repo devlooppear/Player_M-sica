c77692b409ee8156b3b99b83552b8bb01c2f9258
devlooppear <115846304+devlooppear@users.noreply.github.com>
2023-02-16 19:54:32 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# Player_Musica
+# Player_Música
+
 - Esse projeto é um player de música: que carrega, randomiza, para, dá play, cria um loop da mesma música, gerencia e apresenta o tempo corrido e o tempo que falta das musica, de forma relativamente semelhante ao "Spotify".
+
+- O arquivo de execussão é o `index.html`.
+
+- O output é a página `Document`, aberta pelo browser.
+
+## Preview
+
+- Print do output da página no browser como exemplo:
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div align-items="center">
+        <img src="images/prints.png" width="450px" height= "700px"></img>
+    </div>
+</body>
+</html>
+
+## Ferramentas Utilizadas
+
+- Bootstrap.
+
+# In English
+
+# Music_Player
+
+- This project is a music player, which: loads, randomizes, stops, plays, creates a loop of the same music, manages and presents the elapsed time and the remaining time of the songs. Is relatively similar to "Spotify".
+
+- The execution file is `index.html`.
+
+- The output is the `Document` page, opened by the browser.
+
+
+## Preview
+
+- Print the output of the page in the browser as an example:
+
+## Used Tools
+
+- Bootstrap.
---
+++ script.js
@@ -0,0 +1,173 @@
+const songName = document.getElementById('song-name');
+const bandName = document.getElementById('band-name');
+const song = document.getElementById('audio');
+const cover = document.getElementById('cover');
+const play = document.getElementById('play');
+const next = document.getElementById('next');
+const previous = document.getElementById('previous');
+const currentProgress = document.getElementById('current-progress');
+const progressContainer = document.getElementById('progress-container');
+const shuffleButton = document.getElementById('shuffle');
+const repeatButton = document.getElementById('repeat');
+const songTime = document.getElementById('song-time');
+const totalTime = document.getElementById('total-time');
+
+const Someday = {
+    songName: 'Someday',
+    artist: 'The Strokes',
+    file: 'The Strokes - Someday'
+};
+
+const Ambitionz_Az_A_Ridah = {
+    songName: 'Ambitionz Az A Ridah',
+    artist: '2Pac',
+    file: '2Pac - Ambitionz Az A Ridah'
+};
+
+const Earth_Wind_Fire = {
+    songName: 'Let\'s Groove',
+    artist: 'Earth, Wind and Fire',
+    file: 'Earth_Wind_Fire - Let s Groove'
+};
+
+let isPlaying = false;
+let isShuffled = false;
+let repeatOn = false;
+const originalPlaylist = [Someday, Ambitionz_Az_A_Ridah, Earth_Wind_Fire];
+let sortedPlaylist = [...originalPlaylist];
+let index = 0;
+
+function playSong(){
+    play.querySelector('.bi').classList.remove('bi-play-circle-fill');
+    play.querySelector('.bi').classList.add('bi-pause-circle-fill');
+    song.play();
+    isPlaying = true;
+}
+
+function pauseSong(){
+    play.querySelector('.bi').classList.add('bi-play-circle-fill');
+    play.querySelector('.bi').classList.remove('bi-pause-circle-fill');
+    song.pause();
+    isPlaying = false;
+}
+
+function playPauseDecider(){
+    if(isPlaying === true){
+        pauseSong();
+    }
+    else {
+        playSong();
+    }
+}
+
+
+function initializeSong(){
+    cover.src = `images/${sortedPlaylist[index].file}.jpg`;
+    song.src = `songs/${sortedPlaylist[index].file}.mp3`;
+    songName.innerText = sortedPlaylist[index].songName;
+    bandName.innerText = sortedPlaylist[index].artist;
+}
+
+function previousSong(){
+    if(index === 0){
+        index = sortedPlaylist.length - 1;
+    }
+    else {
+        index -= 1;
+    }
+    initializeSong();
+    playSong(); 
+}
+
+function nextSong(){
+    if(index === sortedPlaylist.length - 1){
+        index = 0;
+    }
+    else {
+        index += 1;
+    }
+    initializeSong();
+    playSong(); 
+}
+
+function updateProgress(){
+    const barWidth = (song.currentTime/song.duration)*100;
+    currentProgress.style.setProperty('--progress', `${barWidth}%`);
+    songTime.innerText = toHHMMSS(song.currentTime);
+}
+
+function jumpTo(event){
+    const width = progressContainer.clientWidth;
+    const clickPosition = event.offsetX;
+    const jumpToTime = (clickPosition/width)*song.duration;
+    song.currentTime = jumpToTime;
+}
+
+function shuffleArray(preShuffleArray){
+    let size = sortedPlaylist.length;
+    let currentIndex = size - 1;
+    while(currentIndex > 0){
+        let randomIndex = Math.floor(Math.random()* size);
+        let aux = preShuffleArray[currentIndex];
+        preShuffleArray[currentIndex] = preShuffleArray[randomIndex];
+        preShuffleArray[randomIndex] = aux;
+        currentIndex -= 1;
+    }
+}
+
+function shuffleButtonClicked(){
+    if(isShuffled === false){
+        isShuffled = true;
+        shuffleArray(sortedPlaylist);
+        shuffleButton.classList.add('button-active');
+    }
+    else {
+        isShuffled = false;
+        sortedPlaylist = [...originalPlaylist];
+        shuffleButton.classList.remove('button-active');
+    }
+}
+
+function repeatButtonClicked(){
+    if(repeatOn === false){
+        repeatOn = true;
+        repeatButton.classList.add('button-active');
+    }
+    else {
+        repeatOn = false;
+        repeatButton.classList.remove('button-active');
+    }
+}
+
+function nextOrRepeat() {
+    if (repeatOn === false){
+        nextSong();
+    }
+    else {
+        playSong();
+    }
+}
+
+function toHHMMSS(originalNumber) {
+    let hours = Math.floor(originalNumber/3600);
+    let min = Math.floor((originalNumber - hours * 3600)/60);
+    let secs = Math.floor(originalNumber - hours * 3600 - min * 60);
+
+    return `${hours.toString().padStart(2, '0')}:${min.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
+}
+
+function updateTotalTime() {
+    totalTime.innerText = toHHMMSS(song.duration);
+}
+
+initializeSong();
+
+play.addEventListener('click', playPauseDecider);
+previous.addEventListener('click', previousSong);
+next.addEventListener('click', nextSong);
+song.addEventListener('timeupdate', updateProgress);
+song.addEventListener('ended', nextOrRepeat);
+song.addEventListener('loadedmetadata', updateTotalTime);
+progressContainer.addEventListener('click', jumpTo);
+shuffleButton.addEventListener('click', shuffleButtonClicked);
+repeatButton.addEventListener('click', repeatButtonClicked);
